ee7a77896cbe8b7888e1547a6043f5cd72414a1e
hihimamuLab <hihimamu14@outlook.jp>
2023-01-31 18:03:23 +0900

    Fix token name

--- mamubot.py
+++ mamubot.py
@@ -9,4 +9,4 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
-client.run('MTA2OTc4OTE3NjU5MTA0MDU5Mg.GSiDbp.MHK-gYfwQKUfPaApsOoi0-yraKW2FAH0RTxtUg')
+client.run('token name')
