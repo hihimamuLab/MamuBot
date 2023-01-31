d45acbd9cec3c3ece5bfca36fbd57d95f3e16e32
hihimamuLab <hihimamu14@outlook.jp>
2023-01-31 18:01:36 +0900

    Send startup message

--- mamubot.py
+++ mamubot.py
@@ -1,20 +1,12 @@
 import discord
 
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Ready as {self.user}!')
+        print(discord.__version__)
+
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('/hello'):
-        await message.channel.send('Hello!')
-
-client.run('')
+client = MyClient(intents=intents)
+client.run('MTA2OTc4OTE3NjU5MTA0MDU5Mg.GSiDbp.MHK-gYfwQKUfPaApsOoi0-yraKW2FAH0RTxtUg')
