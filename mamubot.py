bfb93151b7043b9603250c5ce6e72902dc62604e
hihimamuLab <hihimamu14@outlook.jp>
2023-01-31 22:05:02 +0900

    import discord

--- mamubot.py
+++ mamubot.py
@@ -1,12 +1,1 @@
 import discord
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Ready as {self.user}!')
-        print(discord.__version__)
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = MyClient(intents=intents)
-client.run('token name')
